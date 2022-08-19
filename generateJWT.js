a512d42f089ed7c43f160f1da014d82015315fb8
vankongv <vankong.v@gmail.com>
2022-08-19 16:35:01 +1000

    wip to generate JWT

---
+++ generateJWT.js
@@ -0,0 +1,36 @@
+const HMACSHA256 = (stringToSign, secret) => "not_implemented"
+
+// The header typically consists of two parts: 
+// the type of the token, which is JWT, and the signing algorithm being used, 
+// such as HMAC SHA256 or RSA.
+const header = {
+    "alg": "ES256",
+    "kid": "S27C28J257",
+    "typ": "JWT"
+}
+const encodedHeaders = btoa(JSON.stringify(header))
+
+
+// The second part of the token is the payload, which contains the claims.
+// Claims are statements about an entity (typically, the user) and 
+// additional data. There are three types of claims: 
+// registered, public, and private claims.
+const payload = {    
+    "iss": "69a6de91-2a8d-47e3-e053-5b8c7c11a4d1",
+    "iat": 1660876549,
+    "exp": 1660877749,
+    "aud": "appstoreconnect-v1"
+}
+const encodedPlayload = btoa(JSON.stringify(payload))
+
+const privateKey = "asdfadsf"
+
+
+// create the signature part you have to take the encoded header, 
+// the encoded payload, a secret, the algorithm specified in the header, 
+// and sign that.
+const signature = HMACSHA256(`${encodedHeaders}.${encodedPlayload}`, privateKey)
+const encodedSignature = btoa(signature)
+
+const jwt = `${encodedHeaders}.${encodedPlayload}.${encodedSignature}`
+console.log({jwt})
