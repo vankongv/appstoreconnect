2722a77baef556d786ec6ff490cc06aae4596a85
vankongv <vankong.v@gmail.com>
2022-08-19 18:07:16 +1000

    WIP

--- .github/actions/generateJWT/generateJWT.js
+++ .github/actions/generateJWT/generateJWT.js
@@ -1,7 +1,7 @@
 // Unix timestamp in seconds
 const currentTime = parseInt(Date.now()/1000); 
 console.log( currentTime );
-const timeInTenMinutes = parseInt(currentTime + 600);
+const timeInTenMinutes = parseInt(currentTime + 1200); //need to change back to 600 for 10 mins
 console.log( timeInTenMinutes );
 
 const HMACSHA256 = (stringToSign, secret) => "not_implemented"
@@ -27,4 +27,4 @@ const signature = HMACSHA256(`${encodedHeader}.${encodedPayload}`, privateKey)
 const encodedSignature = btoa(signature)
 
 const jwt = `${encodedHeader}.${encodedPayload}.${encodedSignature}`
-console.log({jwt})
+console.log(jwt)
